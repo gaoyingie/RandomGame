7a7ba80760d92cded92d12a911fbeaac71d6bca0
Wang Han <416810799@qq.com>
2016-09-02 10:00:11 +0800

    Support WeChat 6.3.22

--- app/src/main/java/me/veryyoung/wechat/randomgame/VersionParam.java
+++ app/src/main/java/me/veryyoung/wechat/randomgame/VersionParam.java
@@ -8,6 +8,10 @@ public class VersionParam {
 
     public static void init(String version) {
         switch (version) {
+            case "6.3.22":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
+                gameType = "rl";
+                break;
             case "6.3.23":
             case "6.3.25":
                 randomGameClass = "com.tencent.mm.sdk.platformtools.be";
