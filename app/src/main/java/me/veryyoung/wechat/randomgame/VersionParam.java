dfedbc0344f97e1c97a966756c86399dceefeeba
veryyoung <codingyoung@gmail.com>
2017-01-20 20:59:15 +0800

    Support wechat 6.5.4

--- app/src/main/java/me/veryyoung/wechat/randomgame/VersionParam.java
+++ app/src/main/java/me/veryyoung/wechat/randomgame/VersionParam.java
@@ -3,38 +3,48 @@ package me.veryyoung.wechat.randomgame;
 
 public class VersionParam {
 
-    public static String randomGameClass = "com.tencent.mm.sdk.platformtools.be";
-    public static String gameType = "cW";
+    public static String randomGameClass = "com.tencent.mm.sdk.platformtools.bf";
+    public static String gameType = "da";
 
     public static void init(String version) {
         switch (version) {
             case "6.3.22":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "rl";
                 break;
             case "6.3.23":
             case "6.3.25":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "rT";
                 break;
             case "6.3.27":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "tx";
                 break;
             case "6.3.28":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "cv";
                 break;
             case "6.3.30":
-                gameType = "cx";
-                break;
             case "6.3.31":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "cx";
                 break;
             case "6.3.32":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "cC";
                 break;
             case "6.5.3":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.be";
                 gameType = "cW";
                 break;
+            case "6.5.4":
+                randomGameClass = "com.tencent.mm.sdk.platformtools.bf";
+                gameType = "da";
+                break;
             default:
-                gameType = "cW";
+                randomGameClass = "com.tencent.mm.sdk.platformtools.bf";
+                gameType = "da";
         }
     }
 }
